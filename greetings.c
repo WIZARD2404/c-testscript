9feb38f17d59da2129685f704956bbb74f75c0d9
Charles Obinna <65284020+WIZARD2404@users.noreply.github.com>
2022-07-01 13:42:55 +0100

    Create greetings.c

---
+++ greetings.c
@@ -0,0 +1,22 @@
+#include <stdio>
+/**
+ * The main code starts here
+ * This code will accept an input and print my initials, age ,sex
+ */
+int main(void){
+        char a, b, c;
+        int age;
+        long char name, sex;
+        printf ("Enter your name");
+        scanf("%c",&name)
+        printf ("Enter your Initials:");
+        putchar ("%c %c %c \n", &a, &b, &c);
+        printf ("Enter your age:");
+        scanf ("%d \n",&age);
+        printf ("Enter your sex:");
+        scanf("Male or Female:%c \n",&sex);
+        print("Greetings Dear %c of %d years old. You are welcome to eXodus Organiation and we are glad to have you here.\
+        Your initials are %c%c%c .Your are such a wonderful entity to the %c community.", name, age, a, b, csex)
+        return (0);
+}
+        
